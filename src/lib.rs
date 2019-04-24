87c0c82ca143c16c69ddc525e986f322456490cb
Stanislas Plessia <stanislas.plessia@streem.com.au>
2019-04-24 23:15:53 +0200

    Add LLL algorithm for lattice basis reduction (f64 and BigInt impl)

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+//! A Rust implementation of the (basic) Lenstra-Lenstra-Lovasz lattice reduction algorithm
+extern crate rug;
+
+pub mod lll;
+pub mod matrix;
+pub mod vector;
+
+#[cfg(test)]
+mod test {
+    use crate::lll::{lattice_reduce,big_lattice_reduce};
+    use crate::matrix::Matrix;
+    use crate::vector::{VectorF,Vector,BigVector,Dot};
+    use crate::rug::{Integer,Rational};
+
+    #[test]
+    fn test_lll() {
+        let matrix_type: Matrix<VectorF> = Matrix::init(3);
+
+        // "Bad" lattice basis
+        let mut basis = matrix_type.identity();
+        basis.columns[0] = VectorF::from_vector(vec![1., 0., 0., 1345.]);
+        basis.columns[1] = VectorF::from_vector(vec![0., 1., 0., 35.]);
+        basis.columns[2] = VectorF::from_vector(vec![0., 0., 1., 154.]);
+        println!("{:?}", basis);
+
+        // "Good" lattice basis
+        lattice_reduce(&mut basis);
+        println!("{:?}", basis);
+    }
+
+    #[test]
+    fn test_big_lll(){
+         let matrix_type: Matrix<BigVector> = Matrix::init(3);
+
+        // "Bad" lattice basis
+        let mut basis = matrix_type.identity();
+        basis.columns[0] = BigVector::from_vector(vec![Integer::from(1) , Integer::from(0), Integer::from(0), Integer::from(1345)]);
+        basis.columns[1] = BigVector::from_vector(vec![Integer::from(0), Integer::from(1), Integer::from(0), Integer::from(35)]);
+        basis.columns[2] = BigVector::from_vector(vec![Integer::from(0), Integer::from(0), Integer::from(1), Integer::from(154)]);
+        println!("{:?}", basis);
+
+        // "Good" lattice basis
+        big_lattice_reduce(&mut basis);
+        println!("{:?}", basis);
+    }
+}
---
+++ src/lll.rs
@@ -0,0 +1,87 @@
+use crate::matrix::Matrix;
+use crate::vector::{Vector, Dot, VectorF, BigVector};
+use crate::rug::*;
+/**
+ * Lattice reduction (Lenstra-Lenstra-Lovasz algorithm)
+ */
+pub fn lattice_reduce(basis: &mut Matrix<VectorF>) {
+    // Parameter delta in the Lovasz condition
+    let delta = 0.75;
+
+    let n = basis.dimension;
+    let mut swap_condition = true;
+
+    while swap_condition {
+        // Perform rounded Gram-Schmidt orthogonalisation
+        for i in 0..n {
+            for k in 1..i {
+                let j = i - k;
+
+                let b_i = &basis.columns[i];
+                let b_j = &basis.columns[j];
+                let alpha = (b_i.dot(&b_j) / b_j.dot(&b_j)).round();
+                basis.columns[i] = b_i.sub(&b_j.mulf(alpha));
+            }
+        }
+
+        // Check for the Lovasz condition and swap columns if appropriate
+        swap_condition = false;
+        for i in 0..n - 1 {
+            let b_i = &basis.columns[i];
+            let b_ip1 = &basis.columns[i + 1];
+
+            let lhs = delta * b_i.dot(&b_i);
+
+            let alpha = b_ip1.dot(&b_i) / b_i.dot(&b_i);
+            let vec_rhs = b_ip1.add(&b_i.mulf(alpha));
+            let rhs = vec_rhs.dot(&vec_rhs);
+
+            if lhs > rhs {
+                basis.columns.swap(i, i + 1);
+                swap_condition = true;
+                break;
+            }
+        }
+    }
+}
+
+pub fn big_lattice_reduce(basis: &mut Matrix<BigVector>){
+        // Parameter delta in the Lovasz condition
+    let delta = Rational::from((3,4));
+
+    let n = basis.dimension;
+    let mut swap_condition = true;
+
+    while swap_condition {
+        // Perform rounded Gram-Schmidt orthogonalisation
+        for i in 0..n {
+            for k in 1..i {
+                let j = i - k;
+
+                let b_i = &basis.columns[i];
+                let b_j = &basis.columns[j];
+                let alpha = Rational::from((b_i.dot(&b_j) , b_j.dot(&b_j))).round();
+                basis.columns[i] = b_i.sub(&b_j.mulf(alpha));
+            }
+        }
+
+        // Check for the Lovasz condition and swap columns if appropriate
+        swap_condition = false;
+        for i in 0..n - 1 {
+            let b_i = &basis.columns[i];
+            let b_ip1 = &basis.columns[i + 1];
+
+            let lhs =  delta.clone() * Rational::from(b_i.dot(&b_i));
+
+            let alpha = Rational::from((b_ip1.dot(&b_i) , b_i.dot(&b_i))).round();
+            let vec_rhs = b_ip1.add(&b_i.mulf(alpha));
+            let rhs = vec_rhs.dot(&vec_rhs);
+
+            if lhs > rhs {
+                basis.columns.swap(i, i + 1);
+                swap_condition = true;
+                break;
+            }
+        }
+    }
+}
