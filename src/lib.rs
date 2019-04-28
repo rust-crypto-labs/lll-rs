908ebd887a4368495cd2dfc30ef61f48591b7cbe
Shin <edmond.de-roffignac@student.ecp.fr>
2019-04-29 00:57:47 +0200

    Updated tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,46 +8,43 @@ pub mod vector;
 
 #[cfg(test)]
 mod test {
-    use crate::lll::{big_lattice_reduce, lattice_reduce};
+    use crate::l2;
+    use crate::lll;
     use crate::matrix::Matrix;
     use crate::rug::Integer;
     use crate::vector::{BigVector, VectorF};
 
     #[test]
     fn test_lll() {
-        let matrix_type: Matrix<VectorF> = Matrix::init(3);
-
         // "Bad" lattice basis
-        let mut basis = matrix_type.identity();
-        basis.columns[0] = VectorF::from_vector(vec![1., 0., 0., 1345.]);
-        basis.columns[1] = VectorF::from_vector(vec![0., 1., 0., 35.]);
-        basis.columns[2] = VectorF::from_vector(vec![0., 0., 1., 154.]);
+        let mut basis: Matrix<VectorF> = Matrix::init(3, 4);
+        basis[0] = VectorF::from_vector(vec![1., 0., 0., 1345.]);
+        basis[1] = VectorF::from_vector(vec![0., 1., 0., 35.]);
+        basis[2] = VectorF::from_vector(vec![0., 0., 1., 154.]);
         println!("{:?}", basis);
 
         // "Good" lattice basis
-        lattice_reduce(&mut basis);
+        lll::lattice_reduce(&mut basis);
         println!("{:?}", basis);
     }
 
     #[test]
     fn test_big_lll() {
-        let matrix_type: Matrix<BigVector> = Matrix::init(3);
-
         // "Bad" lattice basis
-        let mut basis = matrix_type.identity();
-        basis.columns[0] = BigVector::from_vector(vec![
+        let mut basis: Matrix<BigVector> = Matrix::init(3, 4);
+        basis[0] = BigVector::from_vector(vec![
             Integer::from(1) << 100000,
             Integer::from(0),
             Integer::from(0),
             Integer::from(1345),
         ]);
-        basis.columns[1] = BigVector::from_vector(vec![
+        basis[1] = BigVector::from_vector(vec![
             Integer::from(0),
             Integer::from(1),
             Integer::from(0),
             Integer::from(35),
         ]);
-        basis.columns[2] = BigVector::from_vector(vec![
+        basis[2] = BigVector::from_vector(vec![
             Integer::from(0),
             Integer::from(0),
             Integer::from(1),
@@ -56,7 +53,50 @@ mod test {
         println!("{:?}", basis);
 
         // "Good" lattice basis
-        big_lattice_reduce(&mut basis);
+        lll::big_lattice_reduce(&mut basis);
+        println!("{:?}", basis);
+    }
+
+    #[test]
+    fn test_l2() {
+        // "Bad" lattice basis
+        let mut basis: Matrix<VectorF> = Matrix::init(3, 4);
+        basis[0] = VectorF::from_vector(vec![1., 0., 0., 1345.]);
+        basis[1] = VectorF::from_vector(vec![0., 1., 0., 35.]);
+        basis[2] = VectorF::from_vector(vec![0., 0., 1., 154.]);
+        println!("{:?}", basis);
+
+        // "Good" lattice basis
+        l2::lattice_reduce(&mut basis, 0.5005, 0.999);
+        println!("{:?}", basis);
+    }
+
+    #[test]
+    fn test_big_l2() {
+        // "Bad" lattice basis
+        let mut basis: Matrix<BigVector> = Matrix::init(3, 4);
+        basis[0] = BigVector::from_vector(vec![
+            Integer::from(1) << 100000,
+            Integer::from(0),
+            Integer::from(0),
+            Integer::from(1345),
+        ]);
+        basis[1] = BigVector::from_vector(vec![
+            Integer::from(0),
+            Integer::from(1),
+            Integer::from(0),
+            Integer::from(35),
+        ]);
+        basis[2] = BigVector::from_vector(vec![
+            Integer::from(0),
+            Integer::from(0),
+            Integer::from(1),
+            Integer::from(154),
+        ]);
+        println!("{:?}", basis);
+
+        // "Good" lattice basis
+        l2::big_lattice_reduce(&mut basis, 0.5005, 0.999);
         println!("{:?}", basis);
     }
 }
